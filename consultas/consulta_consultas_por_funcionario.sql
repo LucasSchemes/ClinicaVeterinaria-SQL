06780fff4d3ba9326ddfc7d2ded894a8fb33a8c5
Lucas Saft Schemes <135075935+LucasSchemes@users.noreply.github.com>
2025-06-29 23:16:34 -0300

    Delete consultas/consulta_consultas_por_funcionario.sql

--- consultas/consulta_consultas_por_funcionario.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    f.nome AS Funcionario,
-    COUNT(c.id_con) AS Total_Consultas
-FROM tb_funcionario f
-JOIN tb_consulta c ON f.id_fun = c.id_fun_fkc
-JOIN tb_pet p ON c.id_pet_fkc = p.id_pet
-GROUP BY f.nome
-ORDER BY Total_Consultas DESC;
